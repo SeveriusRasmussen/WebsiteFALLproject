d827370676dfb6b9e071dbbc397b062f03db6b32
SeveriusRasmussen <sora0001@stud.kea.dk>
2025-10-20 01:50:09 +0200

    rollbacked til original som virker lokalt.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // (ESM)
 import express from "express";
 import path from "path";
-import fs from 'fs'; // Til brug i addStaticRoutes function
+import fs from "fs"; // Til brug i addStaticRoutes function
 import { fileURLToPath } from "url";
 import MarkdownIt from "markdown-it";
 
@@ -9,8 +9,8 @@ const md = new MarkdownIt();
 const app = express();
 
 // __dirname i ESM: (to måder)
-//const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(fileURLToPath(import.meta.url)); 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename); 
 
 
 // statiske filer via express (dårligt, brug smarte metoden nedunder)
@@ -20,17 +20,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// Helper: læs md-fil (throw hvis ikke findes)
-/*
-function readMarkdown(slug) {
-    // Tillad kun a-zz0-9-_
-    if (!/^[a-z0-9_-]+$/i.test(slug)) throw new Error("Invalid slug");
-  const full = path.join(__dirname, "docs-md", `${slug}.md`);
-  return fs.readFileSync(full, "utf8");
-}
-  */
 
-// Forside Route: liste over dokumenter (læst fra docs-md)
+// Forside Route: list all .md
 app.get("/", (_req, res) => {
     const dir = path.join(__dirname, "docs-md");
     const files = fs.readdirSync(dir)
@@ -62,6 +53,6 @@ app.get("/docs/:slug", (req, res, next) => {
 });
 
 // Simple 404:
-app.use((_req, res) => res.status(404).render("layout", { title: "Not found", body: "<p>Not found</p>" }));
+//app.use((_req, res) => res.status(404).render("layout", { title: "Not found", body: "<p>Not found</p>" }));
 
 export default app;
